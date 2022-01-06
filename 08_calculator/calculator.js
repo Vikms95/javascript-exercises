5627b2d7136edc0a845e42865bb30fcee363988a
Vikms <vms1995@hotmail.com>
2022-01-06 22:11:11 +0100

    Complete calculator up until factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(a,b){
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b){
+  return a-b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array){
+  if(array.length == 0)
+  {
+    return array.length;
+	  
+  }
+  else if(array.length==1)
+  {
+    return array[0];
+  }
+  else
+  {
+    return array.reduce((total,number) => total + number);
+  }
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total,number) => total * number);
 
 };
 
-const power = function() {
-	
+const power = function(number,power) {
+  return Math.pow(number,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+
+  let factorial = 1;
+  for (let i = 1; i < number; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
