e8ae4a8b682b1cd2fa606233d85719e2d2e29697
Vikms95 <vms1995@hotmail.com>
2022-01-07 05:08:47 +0100

    Finish fibonacci exercice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n){
+    let current = 1;
+    let storedPrevious = 0;
+    let previous = 0;
+    if (Math.sign(n) == -1){
+        return "OOPS";  
+    }
+    else
+    {
+        for (let i = 0; i <= +n - 1; i++) {
+            storedPrevious = current;
+            current = current + previous;
+            previous = storedPrevious;
+        }
+        return previous;
+    }   
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
